4f87c9fcd9749ca91c7e515d7538e4c2c1fcadb2
maiquelb <maiquelb@gmail.com>
2025-12-18 23:10:13 -0300

    update launch_ros

--- examples/turtlebot3/launch_ros.sh
+++ examples/turtlebot3/launch_ros.sh
@@ -1,16 +1,3 @@
-# ```
-# (docker ps -q --filter "name=novnc" | grep -q . && docker stop novnc || true) &&\
-# (docker ps -q --filter "name=noetic" | grep -q . && docker stop noetic || true) &&\
-# sudo docker run -d --rm --net=ros --env="DISPLAY_WIDTH=3000" --env="DISPLAY_HEIGHT=1800" --env="RUN_XTERM=no" --name=novnc -p=8080:8080 theasp/novnc:latest &&\
-# sudo docker run -it -p11311:11311 -p9090:9090 --rm --net=ros --env="DISPLAY=novnc:0.0" --name noetic maiquelb/embedded-mas-ros:latest \
-# /bin/bash -c "source /opt/ros/noetic/setup.bash && \
-# (export TURTLEBOT3_MODEL=burger && roslaunch turtlebot3_gazebo turtlebot3_world.launch > /dev/null 2>&1 &) && \
-# (sleep 5 && roslaunch rosbridge_server rosbridge_websocket.launch > /dev/null 2>&1 &) && \
-# sleep 5 && \
-# echo -e '\e[1;33m**** Docker container is ready. Start the JaCaMo application ****\e[0m' && \
-# tail -f /dev/null"
-# ```
-
 #!/bin/bash
 set -e
 
@@ -29,9 +16,10 @@ sudo docker run -d --rm --net=ros     --env="DISPLAY_WIDTH=3000"     --env="DISP
 
 sudo docker run --rm -d --net=ros     --env="DISPLAY=novnc:0.0"     --env="ROS_MASTER_URI=http://localhost:11311"     --name embedded-mas-example     -p 9090:9090     maiquelb/embedded-mas-ros:latest      #/bin/bash -c "source /opt/ros/noetic/setup.bash &&  \
 
+echo -e "\e[1;33m**** Launching ROS 2 container. Wait 5 seconds ****\e[0m"
 
 sudo docker exec -d embedded-mas-example /bin/bash -c "source /opt/ros/noetic/setup.bash && (export TURTLEBOT3_MODEL=burger && roslaunch turtlebot3_gazebo turtlebot3_world.launch) "
 
 sleep 5
 
-echo -e "\e[1;33m**** Docker container is ready. Open http://localhost:8080/vnc.html ****\e[0m"
+echo -e '\e[1;33m**** Docker container is ready. Start the JaCaMo application and open http://localhost:8080/vnc.html ****\e[0m'
